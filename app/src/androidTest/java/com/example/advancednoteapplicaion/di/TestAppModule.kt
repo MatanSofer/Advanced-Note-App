739f68e46894f1643cfb54b3b78aea23814e2f09
MatanSofer <sofermatan123>
2023-10-06 14:24:26 +0300

    test implement

---
+++ app/src/androidTest/java/com/example/advancednoteapplicaion/di/TestAppModule.kt
@@ -0,0 +1,46 @@
+package com.example.advancednoteapplicaion.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.advancednoteapplicaion.feature_note.data.data_source.NoteDatabase
+import com.example.advancednoteapplicaion.feature_note.data.repository.NoteRepositoryImpl
+import com.example.advancednoteapplicaion.feature_note.domain.repository.NoteRepository
+import com.example.advancednoteapplicaion.feature_note.domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+//one module for feature
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            app,
+            NoteDatabase::class.java,
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository{
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases{
+        return NoteUseCases(
+            getNotes = GetNotesUseCase(repository),
+            deleteNote = DeleteNoteUseCase(repository),
+            addNote = AddNoteUseCase(repository),
+            getNote = GetNoteUseCase(repository),
+        )
+    }
+}
